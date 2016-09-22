8ac28cdb56fb318fa0ecdcdb602e242ffe931aaf
Matthew Herz <matthewaherz@gmail.com>
2016-09-21 19:29:55 -0700

    Make write method public

--- APNGKit/APNGImage.swift
+++ APNGKit/APNGImage.swift
@@ -258,7 +258,7 @@ open class APNGImage: NSObject { // For ObjC compatibility
         }
     }
     
-    func write(to url: URL) throws {
+    public func write(to url: URL) throws {
         var assembler = Assembler(url: url, apng: self)
         do {
             try assembler?.save()
