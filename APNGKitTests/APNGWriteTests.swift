2ac2b75e6f20e108b1b618aa9fac5dee3c99ad02
Matthew Herz <matthewaherz@gmail.com>
2016-09-21 19:30:12 -0700

    Tidy tests

--- APNGKitTests/APNGWriteTests.swift
+++ APNGKitTests/APNGWriteTests.swift
@@ -38,7 +38,6 @@ class APNGWriteTests: XCTestCase {
         try! newPNGImage?.write(to: fileURL)
         
         let imageExists = FileManager.default.fileExists(atPath: path)
-        print("Path: \(path)")
         let savedImage = UIImage(contentsOfFile: path)
         XCTAssertTrue(imageExists, "Saved file should be created.")
         XCTAssertNotNil(savedImage, "Saved image should be readable as image.")
@@ -56,10 +55,55 @@ class APNGWriteTests: XCTestCase {
         try! newPNGImage?.write(to: fileURL)
         
         let imageExists = FileManager.default.fileExists(atPath: path)
-        print("Path: \(path)")
         let savedImage = UIImage(contentsOfFile: path)
         XCTAssertTrue(imageExists, "Saved file should be created.")
         XCTAssertNotNil(savedImage, "Saved image should be readable as image.")
+    }
+    
+    func testUIImageScale() {
+        let fileName = ("elephant_apng@2x", "apng")
+        let retinaAPNG = APNGImage(named: fileName.0)
+        XCTAssertNotNil(retinaAPNG, "elephant_apng should be able to init at 2x.")
+        XCTAssertEqual(retinaAPNG?.scale, 2, "Retina version should be loaded")
+        
+        let dir = NSTemporaryDirectory() as NSString
+        let path = dir.appendingPathComponent("\(fileName.0).\(fileName.1)")
+        let fileURL = URL(fileURLWithPath: path)
+        try! retinaAPNG?.write(to: fileURL)
+        
+        let imageExists = FileManager.default.fileExists(atPath: path)
+        print("Path: \(path)")
+        let savedImage = APNGImage(contentsOfFile: path)
+        XCTAssertTrue(imageExists, "Saved file should be created.")
+        XCTAssertNotNil(savedImage, "Saved image should be readable as image.")
+        
+        XCTAssertEqual(savedImage?.scale, 2, "Retina version should be loaded")
+        XCTAssertEqual(savedImage?.size, CGSize(width: 240, height: 200), "Size should be in point, not pixel.")
+    }
+    
+    func testAPNGWriteCompression() {
+        let bundle = Bundle(for: type(of: self))
+        let fileName = ("spinfox", "apng")
+        let imagePath = bundle.path(forResource: fileName.0, ofType: fileName.1)!
+        let newPNGImage = APNGImage(contentsOfFile: imagePath)
+        
+        let dir = NSTemporaryDirectory() as NSString
+        let path = dir.appendingPathComponent("\(fileName.0).\(fileName.1)")
+        let fileURL = URL(fileURLWithPath: path)
+        try! newPNGImage?.write(to: fileURL)
+        
+        do {
+            let attr : [FileAttributeKey: Any] = try FileManager.default.attributesOfItem(atPath: imagePath)
+            let fileSize = attr[FileAttributeKey.size] as! Double
+
+            let attrNew: [FileAttributeKey: Any] = try FileManager.default.attributesOfItem(atPath: path)
+            let fileSizeNew = attrNew[FileAttributeKey.size] as! Double
+            let percentage: Double = fileSizeNew / fileSize
+            XCTAssertLessThan(fileSizeNew, fileSize)
+            XCTAssertLessThan(percentage, 0.9)
+        } catch {
+            print("Error: \(error)")
+        }
     }
 }
 
